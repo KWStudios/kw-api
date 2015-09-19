8235b72e2a90d3e932fc02c87946b750fbebc282
Ybrin <koray@koska.at>
2015-09-19 14:05:44 +0200

    Removed warnings from linters.

--- app.rb
+++ app.rb
@@ -4,37 +4,40 @@ require 'tilt/haml'
 require 'json'
 require 'digest/sha2'
 
-dbFile = open("json/db.json")
-dbJson = dbFile.read
-db = JSON.parse(dbJson)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://#{db['username']}:#{db['password']}@#{db['hostname']}/#{db['database']}")
+db_file = open('json/db.json')
+db_json = db_file.read
+db = JSON.parse(db_json)
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://#{db['username']}:
+#{db['password']}@#{db['hostname']}/#{db['database']}")
 
+# The Version representing database class
 class Version
   include DataMapper::Resource
   property :id,           Serial
-  property :name,         String, :required => true
+  property :name,         String, required: true
   property :version,      String
   property :completed_at, DateTime
 end
 DataMapper.finalize
 
+# The main class for the TravisWebhook
 class TravisWebhook < Sinatra::Base
   set :token, ENV['TRAVIS_USER_TOKEN']
 
   post '/travis/webhooks/RageMode' do
-    if not valid_request?
+    if !valid_request?
       puts "Invalid payload request for repository #{repo_slug}"
     else
       payload = JSON.parse(params[:payload])
       number = payload['number']
-      version = Version.first_or_create(:name => "#{repo_slug.downcase}")
+      version = Version.first_or_create(name: "#{repo_slug.downcase}")
       version.version = number
       version.completed_at = Time.now
       version.save
-      puts "Authenticated successfully!"
-      puts "Welcome, TravisCI!"
-      puts "Changing stuff on KWStudios/RageMode repo_slug"
-      puts "Received valid payload for repository #{repo_slug}"
+      puts 'Authenticated successfully!'
+      puts 'Welcome, TravisCI!'
+      puts 'Changing stuff on KWStudios/RageMode repo_slug'
+      puts 'Received valid payload for repository #{repo_slug}'
     end
   end
 
@@ -52,14 +55,17 @@ class TravisWebhook < Sinatra::Base
   end
 
   get '/plugins/:user/:repo/versions/newest' do
-    version = Version.first_or_create(:name => "#{params[:user].downcase}/#{params[:repo].downcase}")
+    version = Version.first_or_create(name: "#{params[:user].downcase}/
+    #{params[:repo].downcase}")
     if version.version.nil?
-      "This api has not any data stored yet :/"
+      'This api has not any data stored yet :/'
     else
-      updaterFile = open("json/#{params[:repo].downcase}.json")
-      updaterJson = updaterFile.read
-      updater = JSON.parse(updaterJson)
-      redirect to("http://storage.googleapis.com/play-kwstudios-org/#{params[:repo].downcase}/travis-builds/#{version.version}/#{params[:repo].downcase}-#{updater['VERSION']}.jar")
+      updater_file = open("json/#{params[:repo].downcase}.json")
+      updater_json = updater_file.read
+      updater = JSON.parse(updater_json)
+      redirect to("http://storage.googleapis.com/play-kwstudios-org/
+      #{params[:repo].downcase}/travis-builds/#{version.version}/
+      #{params[:repo].downcase}-#{updater['VERSION']}.jar")
     end
   end
 
