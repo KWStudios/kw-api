3fe7043bd0cd2885c6f63e230a7baa2f0b30123b
Ybrin <koray@koska.at>
2015-09-30 20:58:24 +0200

    Changed path to json file.

--- models/init.rb
+++ models/init.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 require 'json'
 
-db_file = open('../json/db.json')
+db_file = open(File.expand_path('../json/db.json', File.dirname(__FILE__)))
 db_json = db_file.read
 db = JSON.parse(db_json)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://#{db['username']}:"\
