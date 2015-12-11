3d13faa1e7c8dfe0c809517f6ef8c8af6576fdee
Ybrin <koray@koska.at>
2015-12-11 18:55:38 +0100

    Finish login route

--- routes/walkmydog/login.rb
+++ routes/walkmydog/login.rb
@@ -16,7 +16,47 @@ class KWApi < Sinatra::Base
     email = payload['email']
     password = payload['password']
 
+    parameter_array = [email, password]
+    if parameter_array.include?(nil)
+      login_error_json_hash = {
+        message: 'The JSON payload is missing some elements which must be set',
+        error: 422 }
+      login_error_json_string = JSON.generate(login_error_json_hash)
+
+      halt 422, { 'Content-Type' => 'application/json' },
+           login_error_json_string
+    end
+
     profile = Profile.get(email)
-    puts "#{profile.firstname} logged in!"
+    if profile.nil?
+      login_error_json_hash = { message: 'Bad credentials', error: 401 }
+      login_error_json_string = JSON.generate(login_error_json_hash)
+
+      halt 401, { 'Content-Type' => 'application/json' },
+           login_error_json_string
+    end
+
+    password_hash = BCrypt::Password.new(profile.password_hash)
+    if password_hash == password
+      status 200
+      login_success_json_hash = { firstname: profile.firstname,
+                                  lastname: profile.lastname,
+                                  email: profile.email,
+                                  cell_phone_number: profile.cell_phone_number,
+                                  street_address: profile.street_address,
+                                  apartment_number: profile.apartment_number,
+                                  city: profile.city,
+                                  zip_code: profile.zip_code, pets: [] }
+      login_success_json_string = JSON.generate(login_success_json_hash)
+
+      content_type 'application/json'
+      login_success_json_string
+    else
+      login_error_json_hash = { message: 'Bad credentials', error: 401 }
+      login_error_json_string = JSON.generate(login_error_json_hash)
+
+      halt 401, { 'Content-Type' => 'application/json' },
+           login_error_json_string
+    end
   end
 end
