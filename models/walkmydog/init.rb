7fc27f9ba403c0ba4cc86c609eb3343ec3164cc6
Ybrin <koray@koska.at>
2015-12-25 16:43:51 +0100

    Change json path for database files

--- models/walkmydog/init.rb
+++ models/walkmydog/init.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
-db_file = open(File.expand_path('../json/walk_db.json', File.dirname(__FILE__)))
+db_file = open(File.expand_path('../../json/walk_db.json',
+                                File.dirname(__FILE__)))
 db_json = db_file.read
 db = JSON.parse(db_json)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://#{db['username']}:"\
