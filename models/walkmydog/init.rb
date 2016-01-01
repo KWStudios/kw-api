fa16e6f4cc7a07c7ed6eb1599897ab9ba3c9e85c
Ybrin <koray@koska.at>
2016-01-01 18:51:32 +0100

    Add missing require fields for geocoding models

--- models/walkmydog/init.rb
+++ models/walkmydog/init.rb
@@ -1,3 +1,9 @@
 # encoding: utf-8
 require_relative 'profile'
 require_relative 'installation'
+
+require_relative 'geocoding/gm_response'
+require_relative 'geocoding/gm_result'
+require_relative 'geocoding/gm_address_component'
+require_relative 'geocoding/gm_type'
+require_relative 'geocoding/gm_geometry'
