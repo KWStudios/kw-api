58c0f804f018efb1acf553c99e34247233de4195
Ybrin <koray@koska.at>
2016-02-24 17:28:21 +0100

    Add dog walk route

--- helpers/dog_helpers.rb
+++ helpers/dog_helpers.rb
@@ -48,4 +48,24 @@ module DogHelpers
     walk_success_json_string = JSON.generate(walk_json_hash)
     walk_success_json_string
   end
+
+  def get_dog_walk_json_hash(dog_walk)
+    dog_json_hash = get_dog_profile_json_hash(dog_walk.dogprofile)
+    walk_json_hash = { id: dog_walk.id,
+                       scheduled_time: dog_walk.scheduled_time,
+                       type_of_job: dog_walk.type_of_job,
+                       notes: dog_walk.notes,
+                       was_acknowledged: dog_walk.was_acknowledged,
+                       has_started: dog_walk.has_started,
+                       was_finished: dog_walk.was_finished,
+                       created_at: dog_walk.created_at,
+                       updated_at: dog_walk.updated_at,
+                       dog_profile: dog_json_hash }
+    walk_json_hash
+  end
+
+  def get_dog_walks_for_dog_profile(dog_profile)
+    dog_walks = dog_profile.dogwalks.all
+    dog_walks
+  end
 end
--- routes/walkmydog/get_dog_walks.rb
+++ routes/walkmydog/get_dog_walks.rb
@@ -1,6 +1,6 @@
-# The route for getting dog profiles
+# The route for dog walks
 class KWApi < Sinatra::Base
-  post '/walkmydog/users/pets/?' do
+  post '/walkmydog/users/pets/jobs/?' do
     verify_login(params[:payload])
 
     profile = Profile.get(JSON.parse(params[:payload])['email'])
@@ -11,15 +11,18 @@ class KWApi < Sinatra::Base
       '[]'
     end
 
-    dog_profiles_json_hash = []
+    dog_walks_json_hash = []
     dog_profiles.each do |dog_profile|
-      dog_profiles_json_hash << get_dog_profile_json_hash(dog_profile)
+      dog_walks = get_dog_walks_for_dog_profile(dog_profile)
+      dog_walks.each do |dog_walk|
+        dog_walks_json_hash << get_dog_walk_json_hash(dog_walk)
+      end
     end
 
     status 200
-    dog_profiles_json_string = JSON.generate(dog_profiles_json_hash)
+    dog_walks_json_string = JSON.generate(dog_walks_json_hash)
 
     content_type 'application/json'
-    dog_profiles_json_string
+    dog_walks_json_string
   end
 end
