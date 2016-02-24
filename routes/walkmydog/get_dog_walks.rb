a8a21b6e9f3904aef208c28a197392fa14a52741
Ybrin <koray@koska.at>
2016-02-24 18:30:41 +0100

    Add dog walk route

--- routes/walkmydog/get_dog_walks.rb
+++ routes/walkmydog/get_dog_walks.rb
@@ -25,4 +25,31 @@ class KWApi < Sinatra::Base
     content_type 'application/json'
     dog_walks_json_string
   end
+
+  post '/walkmydog/users/pets/:id/jobs/?' do
+    verify_login(params[:payload])
+
+    profile = Profile.get(JSON.parse(params[:payload])['email'])
+
+    dog_profile_id = params['id']
+
+    halt 422, { 'Content-Type' => 'application/json' },
+         missing_elements_json if dog_profile_id.nil?
+
+    dog_profile = profile.dogprofiles.get(dog_profile_id)
+    halt 401, { 'Content-Type' => 'application/json' },
+         bad_credentials_json if dog_profile.nil?
+
+    dog_walks_json_hash = []
+    dog_walks = get_dog_walks_for_dog_profile(dog_profile)
+    dog_walks.each do |dog_walk|
+      dog_walks_json_hash << get_dog_walk_json_hash(dog_walk)
+    end
+
+    status 200
+    dog_walks_json_string = JSON.generate(dog_walks_json_hash)
+
+    content_type 'application/json'
+    dog_walks_json_string
+  end
 end
