6c1185e953129b9c5ff4e0fae33c62266570d499
Ybrin <koray@koska.at>
2016-04-05 00:31:08 +0200

    Finish get all images route

---
+++ routes/stars/get_images.rb
@@ -0,0 +1,18 @@
+# The main class for getting the images
+class KWApi < Sinatra::Base
+  get '/stars/v1/images/all/?' do
+    images = Starsgcsimage.all
+
+    images_hash = []
+    images.each do |image|
+      images_hash << get_stars_gcs_image_json_hash(image)
+    end
+
+    status 200
+
+    images_success_json_string = JSON.generate(dog_walks_json_hash)
+
+    content_type 'application/json'
+    images_success_json_string
+  end
+end
--- routes/stars/init.rb
+++ routes/stars/init.rb
@@ -1,3 +1,4 @@
 # encoding: utf-8
 require_relative 'register_fb_stars_profile'
 require_relative 'upload_image'
+require_relative 'get_images'
