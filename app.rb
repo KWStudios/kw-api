24b36d999aa13bc68cead89edc38f92621593208
Ybrin <koray@koska.at>
2017-08-01 01:52:55 +0200

    Change travis webhook to new signing method

--- app.rb
+++ app.rb
@@ -1,14 +1,40 @@
+require 'rubygems'
+require 'bundler'
+
+# Load dotenv manually so it is loaded before other gems
+# and all the environment variables are already available
+require 'dotenv'
+Dotenv.load
+
+# Require standard library gems
+require 'base64'
+require 'openssl'
+require 'logger'
+
+Bundler.require
+
 # The main class for the kw-api
 class KWApi < Sinatra::Base
   enable :sessions
 
+  configure :production, :development do
+    enable :logging
+  end
+
+  configure :test do
+    set :logging, ::Logger::ERROR
+  end
+
   configure :production do
     set :haml, ugly: true
     set :clean_trace, true
+
+    set :logging, ::Logger::INFO
   end
 
   configure :development do
     # ...
+    set :logging, ::Logger::DEBUG
   end
 
   configure do
--- routes/travis.rb
+++ routes/travis.rb
@@ -2,30 +2,74 @@
 class KWApi < Sinatra::Base
   set :token, ENV['TRAVIS_USER_TOKEN']
 
+  # Use travis-ci.org as default host if no API_HOST env variable is present
+  DEFAULT_API_HOST = 'https://api.travis-ci.org'
+  API_HOST = ENV.fetch('API_HOST', DEFAULT_API_HOST)
+
   post '/travis/webhooks/versions/?' do
-    if !valid_request?
-      puts "Invalid payload request for repository #{repo_slug}"
-    else
-      payload = JSON.parse(params[:payload])
-      number = payload['number']
-      version = Version.first_or_create(name: "#{repo_slug.downcase}")
-      version.version = number
-      version.completed_at = Time.now
-      version.save
-      puts 'Authenticated successfully!'
-      puts 'Welcome, TravisCI!'
-      puts "Changing stuff on #{repo_slug} repo_slug"
-      puts "Received valid payload for repository #{repo_slug}"
+    begin
+      json_payload = params.fetch('payload', '')
+      signature = request.env['HTTP_SIGNATURE']
+
+      pkey = OpenSSL::PKey::RSA.new(public_key)
+
+      verified = pkey.verify(
+        OpenSSL::Digest::SHA1.new,
+        Base64.decode64(signature),
+        json_payload
+      )
+      msg = nil
+      if verified
+        # Run add version
+        number = json_payload['number']
+        version = Version.first_or_create(name: "#{repo_slug.downcase}")
+        version.version = number
+        version.completed_at = Time.now
+        version.save
+        logger.info 'Authenticated successfully!'
+        logger.info 'Welcome, TravisCI!'
+        logger.info "Changing stuff on #{repo_slug} repo_slug"
+        logger.info "Received valid payload for repository #{repo_slug}"
+        # End Run add version
+
+        status 200
+        msg = {
+          status: 200,
+          error: 'verification succeeded'
+        }
+      else
+        status 400
+        msg = {
+          status: 400,
+          error: 'verification failed'
+        }
+      end
+      JSON.generate(msg)
+    rescue => e
+      logger.info "exception=#{e.class} message=\"#{e.message}\""
+      logger.debug e.backtrace.join("\n")
+
+      status 500
+      msg = {
+        status: 500,
+        error: 'exception encountered while verifying signature'
+      }
+      JSON.generate(msg)
     end
   end
 
-  def valid_request?
-    digest = Digest::SHA2.new.update("#{repo_slug}#{settings.token}")
-    digest.to_s == authorization
-  end
+  def public_key
+    request = Typhoeus::Request.new(
+      "#{API_HOST}/config",
+      method: :get,
+      headers: { Accept: 'application/json' }
+    )
+    request.run
+    resp = request.response
 
-  def authorization
-    env['HTTP_AUTHORIZATION']
+    JSON.parse(resp.body)['config']['notifications']['webhook']['public_key']
+  rescue
+    ''
   end
 
   def repo_slug
